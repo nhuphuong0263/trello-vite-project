e6ed5cbc7662aecc5b02913ce8e8c5901a6da649
nhuphuong0263 <nhuphuong0263@users.noreply.github.com>
2023-08-10 15:46:43 +0700

    setup mui-ui & config eslint for path of import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Button from '@mui/material/Button'
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
+import Icon from '@mui/material/Icon'
+import IconButton from '@mui/material/IconButton'
+import SvgIcon from '@mui/material/SvgIcon'
+import { green } from '@mui/material/colors'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" rel="noreferrer" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" rel="noreferrer" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>LePhuongDev</div>
+      <Button variant="text">text</Button>
+      <Button variant="contained">contained</Button>
+      <Button variant="outlined">outlined</Button>
+      <AccessAlarmIcon />
+      <Icon>star</Icon>
+
+      <IconButton aria-label="delete">
+        <SvgIcon>
+          <path d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z" />
+        </SvgIcon>
+      </IconButton>
+
+      <Icon sx={{ color: green[500] }}>add_circle</Icon>
     </>
   )
 }
