3c78ea0c9e9e1298c87edf667cd2aac887075756
nhuphuong0263 <nhuphuong0263@users.noreply.github.com>
2023-08-11 15:12:24 +0700

    added config theme & folder structure

---
+++ src/components/Mode/ModeToggle.jsx
@@ -0,0 +1,15 @@
+import { useColorScheme } from '@mui/material/styles'
+import Button from '@mui/material/Button'
+
+export default function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
