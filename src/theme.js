d71d36e6368c3013fdfb7d20c25024b7d6ae8314
nhuphuong0263 <nhuphuong0263@users.noreply.github.com>
2023-08-14 14:19:10 +0700

    app-bar-s1

--- src/theme.js
+++ src/theme.js
@@ -1,25 +1,66 @@
 import { experimental_extendTheme as extendTheme} from '@mui/material/styles'
-import { teal, deepOrange, cyan, orange } from '@mui/material/colors'
+import { teal, pink, cyan, orange, deepPurple } from '@mui/material/colors'
 
 const theme = extendTheme({
   trello: {
-    appBarHeight: '48px',
-    boardBarHeight: '58px'
+    appBarHeight: '58px',
+    boardBarHeight: '60px'
   },
   colorSchemes: {
     light: {
       palette: {
-        primary: teal,
-        secondary: cyan
+        primary: pink,
+        secondary: teal
       },
       spacing: (factor) => `${0.25 * factor}rem`
     },
     dark: {
       palette: {
-        primary: deepOrange,
+        primary: cyan,
         secondary: orange
       },
       spacing: (factor) => `${0.25 * factor}rem`
+    }
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'capitalize'
+        }
+      }
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: ({ theme }) => {
+          return {
+            color: theme.palette.primary.main,
+            fontSize: '0.875rem'
+          }
+        }
+      }
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: ({ theme }) => {
+          // console.log('theme', theme)
+          return {
+            color: theme.palette.primary.main,
+            fontSize: '0.875rem',
+            '.MuiOutlinedInput-notchedOutline': {
+              borderColor: theme.palette.primary.light
+            },
+            '&:hover': {
+              '.MuiOutlinedInput-notchedOutline': {
+                borderColor: theme.palette.primary.main
+              }
+            },
+            '& fieldset': {
+              borderWidth: '1px !important'
+            }
+          }
+        }
+      }
     }
   }
 })
